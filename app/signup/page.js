92292a535059eda2b0e491907699eaac0e4cf524
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-02-17 13:04:30 +0100

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -6,6 +6,9 @@ export default function Signup() {
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <button className="btn btn-primary">Sign Up</button>
+        <p style={{ marginTop: '20px', textAlign: 'center', color: '#ccc', fontSize: '0.9rem' }}>
+          Already have an account? <a href="/login" style={{ color: '#7f5af0', textDecoration: 'none' }}>Login</a>
+        </p>
       </div>
     </div>
   );
