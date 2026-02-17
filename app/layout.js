2bbad037d74b6f45908e10b596553434d721dc13
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-02-17 12:22:07 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,14 @@
 import "../styles/globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "StackIQ",
-  description: "AI-Verified Backend Skills.",
+  description: "AI-powered backend developer verification",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-slate-800`}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
