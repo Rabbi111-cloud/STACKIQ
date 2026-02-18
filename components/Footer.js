91b9c72a44a834611197517b1646a64c63c7499e
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-02-18 12:07:22 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,8 @@
+"use client";
+
 export default function Footer() {
   return (
-    <footer>
+    <footer style={{ textAlign: "center", padding: "20px 0" }}>
       © 2026 StackVerified. All rights reserved.
     </footer>
   );
