3f47d18fcf6ff3132805cd0e18138d68bb9ee8df
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-02-18 12:09:08 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,14 @@
+"use client";
+
 export default function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className="navbar" style={{ padding: "20px", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
       <h1>StackVerified</h1>
       <div>
-        <a href="#features">Features</a>
-        <a href="#pricing">Pricing</a>
-        <a href="/login">Login</a>
-        <a href="/signup" className="btn btn-primary">Get Started</a>
+        <a href="#features" style={{ margin: "0 10px" }}>Features</a>
+        <a href="#pricing" style={{ margin: "0 10px" }}>Pricing</a>
+        <a href="/login" style={{ margin: "0 10px" }}>Login</a>
+        <a href="/signup" className="btn btn-primary" style={{ margin: "0 10px" }}>Get Started</a>
       </div>
     </nav>
   );
