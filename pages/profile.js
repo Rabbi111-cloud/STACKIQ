fcf8a24396ee0952965b4e7dc68faea8e47582ed
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-02-18 14:36:03 +0100

    Delete pages directory

--- pages/profile.js
+++
@@ -1,9 +0,0 @@
-// pages/profile.js
-import dynamic from "next/dynamic";
-
-// Dynamically import the Profile component and disable SSR
-const ProfilePage = dynamic(() => import("../components/Profile"), { ssr: false });
-
-export default function Profile() {
-  return <ProfilePage />;
-}
